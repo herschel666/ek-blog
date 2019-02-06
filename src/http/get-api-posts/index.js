0c04b105dd45f9e2f5a1c4f986e875009d6ad185
Emanuel Kluge <mail@emanuel-kluge.de>
2019-02-06 22:06:11 +0100

    feat: add pagination to posts API

--- src/http/get-api-posts/index.js
+++ src/http/get-api-posts/index.js
@@ -1,5 +1,7 @@
 const data = require('@architect/data');
 
+const ITEMS_PER_PAGE = 3;
+
 const getBlogposts = async (projectionExpression, options) => {
   const result = await data.blog.query({
     KeyConditionExpression: 'kind = :kind',
@@ -13,21 +15,42 @@ const getBlogposts = async (projectionExpression, options) => {
   return result;
 };
 
+const getLastEvaluatedKey = async (offset) => {
+  if (offset === 0) {
+    return null;
+  }
+  const { LastEvaluatedKey: lastEvaluatedKey } = await getBlogposts('uid', {
+    ScanIndexForward: false,
+    Limit: offset,
+    ExclusiveStartKey: null,
+  });
+  return lastEvaluatedKey;
+};
+
 exports.handler = async (req) => {
   console.log();
   console.log(req);
 
   try {
-    const [{ Items: posts = [] }, { Count: count }] = await Promise.all([
+    const currentPage = Number(req.query.page || '1');
+    const offset = (currentPage - 1) * ITEMS_PER_PAGE;
+    const lastEvaluatedKey = await getLastEvaluatedKey(offset);
+    const [
+      { Items: posts = [], LastEvaluatedKey: hasNextPage },
+      { Count: count },
+    ] = await Promise.all([
       getBlogposts('uid, content, title, slug, createdAt', {
         ScanIndexForward: false,
+        Limit: ITEMS_PER_PAGE,
+        ExclusiveStartKey: lastEvaluatedKey,
       }),
       getBlogposts('uid', { Select: 'COUNT' }),
     ]);
+    const nextPage = hasNextPage ? currentPage + 1 : null;
 
     return {
       type: 'application/json',
-      body: JSON.stringify({ posts, count }),
+      body: JSON.stringify({ posts, count, nextPage }),
     };
   } catch (err) {
     console.log(err);
