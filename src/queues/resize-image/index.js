82ff41a2156dd745e2b1774f181789a35140b489
Emanuel Kluge <mail@emanuel-kluge.de>
2019-02-16 16:40:33 +0100

    wip: consider all sizes when resizing images

--- src/queues/resize-image/index.js
+++ src/queues/resize-image/index.js
@@ -2,9 +2,22 @@ const arc = require('@architect/functions');
 const { S3 } = require('aws-sdk');
 const gm = require('gm').subClass({ imageMagick: true });
 const { writeFile } = require('@architect/shared/util');
-const { IMAGE_SIZES } = require('@architect/shared/constants');
+const {
+  IMAGE_SIZES,
+  IMAGE_SIZE_THUMB,
+} = require('@architect/shared/constants');
 
-const handler = (record, callback) => {
+const getOriginalDimensions = (instance) =>
+  new Promise((resolve, reject) => {
+    instance.size((err, originalDimensions) => {
+      if (err) {
+        return reject(err);
+      }
+      resolve(originalDimensions);
+    });
+  });
+
+const handler = async (record, callback) => {
   const { media, size, mime } = record;
   const buffer = new Buffer(media, 'base64');
   const [filename, ext] = record.filename.split('.');
@@ -14,22 +27,31 @@ const handler = (record, callback) => {
     return callback(new Error(`Unknown image size "${size}" given.`));
   }
 
-  // TODO: handle portrait mode images correctly
-  gm(buffer, record.fileName)
-    .resize(...dimensions)
-    .toBuffer(ext.toUpperCase(), (err, resized) => {
-      if (err) {
-        callback(err);
-        return;
-      }
+  const instance = gm(buffer);
+  const { width, height } = await getOriginalDimensions(instance);
 
-      writeFile({
-        s3: new S3(),
-        buffer: resized,
-        filename: `${filename}-${size}.${ext}`,
-        mime,
-      }).then(() => callback(), callback);
-    });
+  if (height > width) {
+    dimensions.reverse();
+  }
+
+  if (size === IMAGE_SIZE_THUMB) {
+    dimensions.push('!');
+  }
+
+  // TODO: handle portrait mode images correctly
+  instance.resize(...dimensions).toBuffer(ext.toUpperCase(), (err, resized) => {
+    if (err) {
+      callback(err);
+      return;
+    }
+
+    writeFile({
+      s3: new S3(),
+      buffer: resized,
+      filename: `${filename}-${size}.${ext}`,
+      mime,
+    }).then(() => callback(), callback);
+  });
 };
 
 exports.handler = arc.queues.subscribe(handler);
