9c84b3638090b08f66533a91103e920ee0f72090
Emanuel Kluge <mail@emanuel-kluge.de>
2019-02-21 22:01:27 +0100

    fix: use correct bucket name in constants

--- src/shared/constants.js
+++ src/shared/constants.js
@@ -1,6 +1,6 @@
-exports.BUCKET_NAME_STAGING = 'ek-blog-media-flDXLaDcW-staging';
+exports.BUCKET_NAME_STAGING = 'ek-blog-media-fldxladcw-staging';
 
-exports.BUCKET_NAME_PROD = 'ek-blog-media-flDXLaDcW';
+exports.BUCKET_NAME_PROD = 'ek-blog-media-fldxladcw';
 
 exports.IMMUTABLE_CACHE_DURATION = 60 * 60 * 24 * 365; // 1 year
 
